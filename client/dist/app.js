2bf0b448b1a59fc39dec80f7720eb17378069b49
Yan Kwong <yantkwong@gmail.com>
2019-11-09 14:45:20 -0800

    added progress bars

--- client/dist/app.js
+++ client/dist/app.js
@@ -13,6 +13,26 @@ STR.crisis = (function () {
         {
             name: 'Procher',
             impact: 15
+        },
+        {
+            name: 'Baby rhino attacked by wild predators',
+            impact: 5
+        },
+        {
+            name: 'wandered away from safe zone',
+            impact: 8
+        },
+        {
+            name: 'deforestation worsen',
+            impact: 10
+        },
+        {
+            name: 'too aggressive to bread',
+            impact: 5
+        },
+        {
+            name: 'mistakenly hunted',
+            impact: 10
         }
     ];
 
