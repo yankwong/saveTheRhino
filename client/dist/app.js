94206575eb5fcb57e1311bbeb6502f14a7df60dc
Yan Kwong <yantkwong@gmail.com>
2019-11-10 01:58:14 -0800

    endgame modal and happy path end game

--- client/dist/app.js
+++ client/dist/app.js
@@ -185,18 +185,23 @@ STR.games = (function () {
         {
             name: 'Fq',
             animate: 'rubberBand'
+        }];
+
+    const endgameText = {
+        win: {
+            title: 'You did it! We could use your help',
+            body: 'Please help by donating'
+        },
+        lose: {
+            title: 'It is an uphill battle but we could use your help',
+            body: 'Sumatran rhino is one of the most endangered mammals on Earth. Each day we lose 1 rhino. Please help by donating.'
         }
-    ];
+    }
 
     let
         currentDay = 0,
         population = 80,
         budget = 1500,
-        getOneRandomRhino = () => {
-            const totalRhino = rhinos.length;
-            const rhinoIndex = STR.utils.getRandomInt(1, totalRhino);
-            return rhinos[rhinoIndex];
-        },
         getAllRhinoPositionsArray = () => {
             const availablePositions = $('.rhino-position').length;
             const numberOfRhinos = STR.constants.maxRhino;
@@ -289,10 +294,33 @@ STR.games = (function () {
         triggerEndgameModal = (userWon) => {
             const $endGameModal = $('#endgame-modal');
             const $modalTitle = $('#endgame-modal .modal-title');
+            const $modalBody = $('#endgame-modal .modal-body p');
+            const endgameTextKey = userWon ? 'win' : 'lose';
+
+            $modalTitle.text(endgameText[endgameTextKey].title);
+            $('#endgame-modal .retry-btn').on('click', () => {
+                window.location.href = "https://yankwong.github.io/saveTheRhino/";
+            });
+            $('#endgame-modal .share-on-fb').on('click', () => {
+                
+            });
+            $('#endgame-modal .adopt-a-rhino').on('click', ()=> {
+                window.location.href = "https://rhinos.org/adopt/";
+            });
+
             if (userWon) {
-                $modalTitle.text('YOU WON');
+                const livingRhino = $('.alive')[0];
+                const livingRhinoName = $(livingRhino).attr('alt');
+                const totalLivingRhino = $('.alive').length;
+                const moreThanOneSurvived = totalLivingRhino > 1 ? true : false;
+
+                let finalText = `Congrats you have saved ${livingRhinoName}`;
+                finalText += moreThanOneSurvived ? ` and ${totalLivingRhino - 1} more rhino. ` : '';
+                finalText += endgameText[endgameTextKey].body;
+
+                $modalBody.text(finalText);
             } else {
-                $modalTitle.text('YOU LOSE');
+                $modalBody.text(endgameText[endgameTextKey].body);
             }
             $endGameModal.modal('show');
         },
